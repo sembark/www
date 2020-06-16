56f784c922f37983cbc3c67fe6708bf8dc1d6e87
Sudhir Mitharwal <luckysud4@gmail.com>
2020-06-16 12:22:18 +0530

    init: basic setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sembark Marketing Website
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+module.exports = {
+  purge: ["./src/**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [require("@tailwindcss/ui")],
+};
