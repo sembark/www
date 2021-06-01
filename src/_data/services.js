140c63d8e2e47f79720526875191d4f3467db686
Sudhir Mitharwal <luckysud4@gmail.com>
2021-06-01 18:20:07 +0530

    fix(chat): remove the message chat

---
+++ src/_data/services.js
@@ -0,0 +1,12 @@
+module.exports = {
+  facebook: {
+    pageId: "104237884797689",
+  },
+  tawk: {
+    accountKey: "60b0f548de99a4282a1a1dd7",
+    widgetId: "1f6pjsa2b",
+  },
+  googleAnalytics: {
+    measurementId: process.env.APP_GA_MEASUREMENT_ID,
+  },
+};
