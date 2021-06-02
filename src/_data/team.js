16bde6486ea37e58a5984c0a7e89e3aa17cd8490
Sudhir Mitharwal <luckysud4@gmail.com>
2021-06-02 20:20:57 +0530

    feat(page): add about us page

---
+++ src/_data/team.js
@@ -0,0 +1,22 @@
+module.exports = {
+  members: [
+    {
+      name: "Anand Singh Shekhawat",
+      designation: "CEO, Co-founder",
+      past: "IIT Alumni",
+      profile_img: "profile_rajat",
+    },
+    {
+      name: "Rajat Sharma",
+      designation: "CSO, Co-founder",
+      past: "NIT Alumni",
+      profile_img: "profile_rajat",
+    },
+    {
+      name: "Sudhir Mitharwal",
+      designation: "CTO, Co-founder",
+      past: "IIT Alumni",
+      profile_img: "profile_sudhir",
+    },
+  ],
+};
